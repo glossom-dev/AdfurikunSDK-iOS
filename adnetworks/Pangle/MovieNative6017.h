3c52f6338c9fe3f2973130fed538ba9a6544ed69
Sungil Kim <sungil.kim@glossom.co.jp>
2020-11-04 11:20:36 +0900

    adf version 3.6.1

---
+++ adnetworks/Pangle/MovieNative6017.h
@@ -0,0 +1,22 @@
+//
+//  MovieNative6017.h
+//  MovieRewardTestApp
+//
+//  Created by Sungil Kim on 2020/10/23.
+//  Copyright © 2020 Glossom, Inc. All rights reserved.
+//
+
+#import <ADFMovieReward/ADFMovieReward.h>
+#import <BUAdSDK/BUAdSDK.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieNative6017 : ADFmyMovieNativeInterface <BUNativeAdDelegate, BUVideoAdViewDelegate>
+
+@end
+
+@interface MovieNativeAdInfo6017 : ADFMovieNativeAdInfo
+
+@end
+
+NS_ASSUME_NONNULL_END
