754faa19747e50dc485232b83952346d0d11a488
Kim, Sungil <masha76@gmail.com>
2023-09-13 13:04:09 +0900

    modify AfiO adapter file

--- adnetworks/Afio/MovieInterstitial6010.swift
+++ adnetworks/Afio/MovieInterstitial6010.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import ADFMovieReward
 
 @objc(MovieInterstitial6010)
 
--- adnetworks/Afio/MovieNative6010.swift
+++ adnetworks/Afio/MovieNative6010.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import AMoAd
+import ADFMovieReward
 
 @objc(MovieNative6010)
 
